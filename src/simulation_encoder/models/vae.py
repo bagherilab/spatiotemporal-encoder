3b7e58f437587047365944519297a08f9ec87ff2
Jacob Evarts <jacob.i.evarts@gmail.com>
2024-06-24 09:51:45 -0700

    initial vae

---
+++ src/simulation_encoder/models/vae.py
@@ -0,0 +1,253 @@
+from typing import Optional, Any
+from collections import defaultdict
+
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from simulation_encoder.logger import ExperimentLogger
+from simulation_encoder.models.abstract_cnn import BaseCNN
+
+
+class VAE(BaseCNN):
+    """
+    Variational Autoencoder class for encoding image data.
+
+    Parameters
+    ----------
+    name : str
+        Name of the model
+    architecture: dict[str: list[dict[str: Any]]]
+        Dictionary containing the architecture of the network
+    num_epochs : int
+        Number of epochs to train the network
+    params : dict[str: Any]
+        Dictionary containing model hyperparameters
+    logger : Logger, optional
+        Logger object for logging, by default None
+    """
+
+    def __init__(
+        self,
+        name: str,
+        architecture: dict[str, list[dict[str, Any]]],
+        num_epochs: int = 5,
+        params: dict[str, Any] = {},
+        logger: Optional[ExperimentLogger] = None,
+    ):
+        super().__init__()
+
+        self.device = self._get_device()
+
+        self.name = name
+        self.architecture = architecture
+        self.num_epochs = num_epochs
+        self.params = params
+        self.logger = logger
+        self.latent_dim = params.get("latent_dim", 32)
+        self.loss_weights = {
+            "image": params.get("image_loss_weight", 1.0),
+            "timepoint": params.get("timepoint_loss_weight", 1.0),
+        }
+
+        self.encoder = nn.Sequential(*self._create_layers(self.architecture["encoder"].copy()))
+        self.fc_mu = nn.Linear(self.architecture["encoder"][-1]["out_features"], self.latent_dim)
+        self.fc_logvar = nn.Linear(self.architecture["encoder"][-1]["out_features"], self.latent_dim)
+        
+        self.decoder_image = nn.Sequential(*self._create_layers(self.architecture["decoder_image"]))
+        self.decoder_timepoint = nn.Sequential(
+            *self._create_layers(self.architecture["decoder_timepoint"])
+        )
+        self.optimizers = {
+            "combined": torch.optim.Adam(self.parameters(), lr=0.001),
+        }
+        self.criterion = {
+            "image": nn.MSELoss(),
+            "timepoint": nn.CrossEntropyLoss(),
+        }
+
+    def fit(
+        self,
+        train_loader: DataLoader,
+        val_loader: Optional[DataLoader] = None,
+    ) -> tuple[dict[str, list[float]], dict[str, list[float]], dict[str, list[float]]]:
+        """
+        Fits the network over the training data for a number of epochs.
+
+        Parameters
+        ----------
+        train_loader : DataLoader
+            DataLoader containing training data
+        epochs : int
+            Number of epochs to train the network
+        val_loader: DataLoader, optional
+            DataLoader containing validation data, by default None
+
+        Returns
+        -------
+        dict[str, list[float]], dict[str, list[float]], dict[str, list[float]]
+            Dicts of training loss, validation loss, and gradient norms
+        """
+        self.to(self.device)
+
+        train_losses: dict[str, list[float]] = defaultdict(list)
+        val_losses: dict[str, list[float]] = defaultdict(list)
+        grad_norms: dict[str, list[float]] = defaultdict(list)
+
+        for e in range(self.num_epochs):
+            train_loss = self.train_one_epoch(train_loader, e)
+            for loss_type, loss in train_loss.items():
+                train_losses[loss_type].append(loss)
+
+            if val_loader:
+                val_loss = self.eval_one_epoch(val_loader)
+                for loss_type, loss in val_loss.items():
+                    val_losses[loss_type].append(loss)
+
+            encoder_grad_norm = self._get_grad_norm(self.encoder)
+            decoder_image_grad_norm = self._get_grad_norm(self.decoder_image)
+            decoder_timepoint_grad_norm = self._get_grad_norm(self.decoder_timepoint)
+
+            grad_norms["encoder"].append(encoder_grad_norm.item())
+            grad_norms["decoder_image"].append(decoder_image_grad_norm.item())
+            grad_norms["decoder_timepoint"].append(decoder_timepoint_grad_norm.item())
+
+            if self.logger:
+                msg = f"Epoch {e+1}/{self.num_epochs}- Train loss: {train_loss['combined']} Val loss: {val_loss['combined']}"
+                self.logger.log(msg)
+
+        return (train_losses, val_losses, grad_norms)
+
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, ...]:
+        """Performs encoding and several decoding heads"""
+        mu, logvar = self.encode(x)
+        z = self.reparameterize(mu, logvar)
+        pred_image = self.decode_image(z)
+        pred_timepoint = self.decode_timepoint(z)
+        return pred_image, pred_timepoint, mu, logvar
+
+    def encode(self, x: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor]:
+        """Encodes input tensor to mean and log variance"""
+        h = self.encoder(x)
+        mu = self.fc_mu(h)
+        logvar = self.fc_logvar(h)
+        return mu, logvar
+
+    def reparameterize(self, mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
+        """Reparameterizes to sample z"""
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode_image(self, z: torch.Tensor) -> torch.Tensor:
+        """Decodes latent tensor to reconstruct image"""
+        return self.decoder_image(z)
+
+    def decode_timepoint(self, z: torch.Tensor) -> torch.Tensor:
+        """Decodes latent tensor to predict sample timepoint"""
+        return self.decoder_timepoint(z)
+
+    def encode_loader(self, dataloader: DataLoader) -> torch.Tensor:
+        """Encodes a loader of data"""
+        self.eval()
+        self.to(self.device)
+
+        encoded = []
+        with torch.no_grad():
+            for inputs, _ in dataloader:
+                inputs = inputs.to(self.device)
+                mu, logvar = self.encode(inputs)
+                z = self.reparameterize(mu, logvar)
+                encoded.append(z)
+        return torch.cat(encoded, dim=0)
+
+    def train_one_epoch(
+        self,
+        train_loader: DataLoader,
+        epoch: int,
+    ) -> dict[str, float]:
+        """
+        Trains the network for one epoch with batches of data.
+
+        Parameters
+        ----------
+        train_loader : DataLoader
+            DataLoader containing training data
+        epoch : int
+            Current epoch number
+
+        Returns
+        -------
+        dict[str, float]
+            Dictionary containing average loss for image, timepoint, and combined
+        """
+        self.train()  # Sets dropout and batch normalization layers to training mode
+
+        optimizer_combined: torch.optim.Optimizer = self.optimizers["combined"]
+        image_criteria: torch.nn.Module = self.criterion["image"]
+        timepoint_criteria: torch.nn.Module = self.criterion["timepoint"]
+
+        avg_loss: dict[str, float] = defaultdict(float)
+
+        with tqdm(train_loader, unit=" batch", ncols=100, desc=f"Epoch {epoch + 1}") as tepoch:
+            for inputs, labels in tepoch:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                optimizer_combined.zero_grad()
+
+                pred_image, pred_timepoint, mu, logvar = self(inputs)
+
+                batch_loss = {
+                    "image": image_criteria(pred_image, inputs),
+                    "timepoint": timepoint_criteria(pred_timepoint, labels),
+                    "kl": self.kl_divergence(mu, logvar)
+                }
+                combined_loss, combined_loss_weighted = self._calc_combined_loss(batch_loss)
+
+                combined_loss_weighted.backward()  # type: ignore
+                optimizer_combined.step()
+
+                for key in batch_loss:
+                    avg_loss[key] += batch_loss[key].item()
+                avg_loss["combined"] += combined_loss.item()
+
+                tepoch.set_postfix(
+                    image_loss=round(batch_loss["image"].item(), 3),
+                    timepoint_loss=round(batch_loss["timepoint"].item(), 3),
+                    kl_loss=round(batch_loss["kl"].item(), 3)
+                )
+
+        avg_loss = {key: value / len(train_loader) for key, value in avg_loss.items()}
+        return avg_loss
+
+    def eval_one_epoch(self, val_loader: DataLoader) -> dict[str, float]:
+        """
+        Validates the network during training with batches of data.
+
+        Parameters
+        ----------
+        val_loader : DataLoader
+            DataLoader containing validation data
+
+        Returns
+        -------
+        dict[str, float]
+            Dictionary containing average loss for image, timepoint, and combined
+        """
+        self.eval()  # Sets dropout and batch normalization layers to evaluation mode
+
+        image_criteria = self.criterion["image"]
+        timepoint_criteria = self.criterion["timepoint"]
+
+        avg_loss: dict[str, float] = defaultdict(float)
+        with torch.no_grad():
+            for inputs, labels in val_loader:
+                inputs, labels = inputs.to(self.device), labels.to(self.device)
+                pred_image, pred_timepoint, mu, logvar = self(inputs)
+
+                batch_loss = {
+                    "image": image_criteria(pred_image, inputs),
+                    "timepoint": timepoint_criteria(pred_timepoint, labels),
+                    "kl": self.kl_divergence(mu, logvar)
+                }
+                combined_loss
