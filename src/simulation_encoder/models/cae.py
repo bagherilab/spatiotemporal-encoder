42b9ea4846a859ed6178b371652a8a488363c2f5
EC2 Default User <ec2-user@ip-172-31-21-116.us-west-2.compute.internal>
2024-10-02 20:53:46 +0000

    immortal dataset updates

--- src/simulation_encoder/models/cae.py
+++ src/simulation_encoder/models/cae.py
@@ -63,7 +63,7 @@ class CAE(BaseCNN):
         }
         self.criterion = {
             "image": nn.MSELoss(),
-            "timepoint": nn.MSELoss(),
+            "timepoint": nn.CrossEntropyLoss(),
         }
 
         # Chosen arbitrarily
@@ -134,10 +134,10 @@ class CAE(BaseCNN):
             optimizer_combined.zero_grad()
 
             pred_image, pred_timepoint = self(inputs)
-            pred_timepoint = pred_timepoint.squeeze().round()
+
             batch_loss = {
                 "image": image_criteria(pred_image, inputs),
-                "timepoint": timepoint_criteria(pred_timepoint, labels.float()),
+                "timepoint": timepoint_criteria(pred_timepoint, labels),
             }
             reconstruction_loss, reconstruction_loss_weighted = self._calc_reconstruction_loss(
                 batch_loss
@@ -182,7 +182,7 @@ class CAE(BaseCNN):
 
                 batch_loss = {
                     "image": image_criteria(pred_image, inputs),
-                    "timepoint": timepoint_criteria(pred_timepoint, labels.float()),
+                    "timepoint": timepoint_criteria(pred_timepoint, labels),
                 }
                 reconstruciton_loss, _ = self._calc_reconstruction_loss(batch_loss)
                 for key in batch_loss:
