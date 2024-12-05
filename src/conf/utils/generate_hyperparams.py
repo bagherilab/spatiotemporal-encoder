5b72dea7ceb807d748f6334180a2bf7788222708
EC2 Default User <jevarts@uw.edu>
2024-12-05 17:43:18 +0000

    remove depreicated model configs

--- src/conf/utils/generate_hyperparams.py
+++ src/conf/utils/generate_hyperparams.py
@@ -39,16 +39,19 @@ def _get_continuous_values(
     if continuous_params:
         for param, param_info in continuous_params.items():
             num_samples = param_info.num_samples
-            if param_info.search == "linear":
-                values = np.linspace(param_info.range[0], param_info.range[1], num_samples).tolist()
-            elif param_info.search == "log":
-                values = np.logspace(
-                    np.log10(param_info.range[0]),
-                    np.log10(param_info.range[1]),
-                    num_samples,
-                ).tolist()
+            if num_samples == 1:
+                values = [param_info.range]
             else:
-                raise ValueError(f"Unsupported search method: {param_info.search}")
+                if param_info.search == "linear":
+                    values = np.linspace(param_info.range[0], param_info.range[1], num_samples).tolist()
+                elif param_info.search == "log":
+                    values = np.logspace(
+                        np.log10(param_info.range[0]),
+                        np.log10(param_info.range[1]),
+                        num_samples,
+                    ).tolist()
+                else:
+                    raise ValueError(f"Unsupported search method: {param_info.search}")
             continuous_values[param] = values
     return continuous_values
 
