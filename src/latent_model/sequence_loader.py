ea9a1a559bd0394ba561689a9ff990c55ad9ab76
jevarts <jevarts@uw.edu>
2025-03-03 15:07:11 -0800

    refactor out encoder model and loader creation

--- src/latent_model/sequence_loader.py
+++ src/latent_model/sequence_loader.py
@@ -7,20 +7,23 @@ from torch.utils.data import DataLoader, Dataset
 
 class TimeSeriesDataset(Dataset):
     """Dataset for time-series or otherwise ordered data"""
+
     def __init__(self, sequences: dict):
         self.sequences = sequences
         self.sample_ids = list(sequences.keys())
-    
+
     def __len__(self):
         return len(self.sample_ids)
-    
+
     def __getitem__(self, idx):
         sample_id = self.sample_ids[idx]
         sequence = torch.tensor(self.sequences[sample_id], dtype=torch.float32)
         return sequence, sample_id
 
-class SequenceLoader():
+
+class SequenceLoader:
     """TODO"""
+
     def __init__(
         self,
         data_path: str,
@@ -29,12 +32,14 @@ class SequenceLoader():
         val_split: float = 0.2,
         test_split: float = 0.2,
         batch_size: int = 16,
-        random_seed: int = 42
+        random_seed: int = 42,
     ):
         self.data = self._load_csv(data_path)
         self.sequence_col = sequence_col
-        self.id_col = id_col 
-        self.feature_cols = [col for col in self.data.columns if col not in [self.sequence_col, self.id_col]]
+        self.id_col = id_col
+        self.feature_cols = [
+            col for col in self.data.columns if col not in [self.sequence_col, self.id_col]
+        ]
         self.num_dims = len(self.feature_cols)
 
         self.val_split = val_split
@@ -60,7 +65,7 @@ class SequenceLoader():
             ids = self._test_ids
         else:
             raise ValueError(f"Invalid dataset type: {dataset_type}")
-        
+
         subset = {id: self.sequences[id] for id in ids}
         dataset = TimeSeriesDataset(subset)
 
@@ -72,10 +77,13 @@ class SequenceLoader():
 
     def _split_data(self) -> tuple[list[int], ...]:
         sample_ids = list(self.sequences.keys())
-        train_ids, test_ids = train_test_split(sample_ids, test_size=self.test_split, random_state=self.random_seed)
-        train_ids, val_ids = train_test_split(train_ids, test_size=self.val_split, random_state=self.random_seed)
+        train_ids, test_ids = train_test_split(
+            sample_ids, test_size=self.test_split, random_state=self.random_seed
+        )
+        train_ids, val_ids = train_test_split(
+            train_ids, test_size=self.val_split, random_state=self.random_seed
+        )
         return train_ids, val_ids, test_ids
 
     def _load_csv(self, data_path: str) -> pd.DataFrame:
         return pd.read_csv(data_path)
-    
--- src/latent_model/temporal_prediction.py
+++ src/latent_model/temporal_prediction.py
@@ -1,17 +1,8 @@
 import os
-import json
 import itertools
 
-import neuralop
-import torch
-from torch.optim import Adam, SGD
-torch.serialization.add_safe_globals([torch._C._nn.gelu])
-torch.serialization.add_safe_globals([neuralop.layers.spectral_convolution.SpectralConv])
-
-from simulation_encoder.plotter import Plotter
-from simulation_encoder.models.base_nn import BaseNN
-from simulation_encoder.main import create_encoder_model
-from simulation_encoder.utils.yaml_utils import load_model_yaml
+from simulation_encoder.models.model_retrieval import load_models
+from simulation_encoder.loaders.loader_retrieval import load_loaders
 
 from latent_model.temporal_models import TemporalModel, RNNModel, LSTMModel
 from latent_model.sequence_loader import SequenceLoader
@@ -20,19 +11,22 @@ from latent_model.runner import Runner
 RESULTS_DIR = "results"
 EXPERIMENT_NAME = "arch_simple_cancer_v3"
 
+
 def main() -> None:
     results_path = f"{RESULTS_DIR}/{EXPERIMENT_NAME}"
     sequential_loaders = create_loaders(results_path)
-    # models = create_models(results_path, sequential_loaders)
+    temporal_models = create_models(results_path, sequential_loaders)
 
-    # runner = Runner()
-    # runner.add_loaders(sequential_loaders)
-    # runner.add_models(models)
+    runner = Runner()
+    runner.add_loaders(sequential_loaders)
+    runner.add_models(temporal_models)
 
-    # results = runner.run_temporal_model()
-    # models = get_encoder_model(results_path)
+    results = runner.run_temporal_model()
 
-    # print(results)
+    encoder_models = load_models(results_path)
+    encoder_loaders = load_loaders(results_path)
+
+    print(results)
 
     # for model_name, dataset in models.items():
     #     for dataset_name, encoder_model in dataset.items():
@@ -49,6 +43,7 @@ def main() -> None:
     #             Plotter.show_images([image_decoded, pred_image_decoded], ["decoded", "predicted"])
     #             break
 
+
 def create_loaders(results_path: str) -> dict[str, dict[str, SequenceLoader]]:
     """Creates data loaders for all of the best performing models in a given encoder result"""
     loaders = {}
@@ -56,14 +51,12 @@ def create_loaders(results_path: str) -> dict[str, dict[str, SequenceLoader]]:
         loaders[model_name] = {}
         model_path = f"{results_path}/{model_name}/_best_model"
         for dataset_name in os.listdir(model_path):
-            loaders[model_name][dataset_name] = create_loader(model_path, dataset_name)
+            loaders[model_name][dataset_name] = SequenceLoader(f"{model_path}/{dataset_name}/encoded_data.csv")
     return loaders
-        
-def create_loader(model_path: str, dataset_name: str) -> SequenceLoader:
-    """Creates a loader for encoded data for a specific model and dataset"""
-    return SequenceLoader(f"{model_path}/{dataset_name}/encoded_data.csv")
 
-def create_models(results_path:str, loaders: dict[str, dict[str, SequenceLoader]]) -> dict[str, dict[str, list[TemporalModel]]]:
+def create_models(
+    results_path: str, loaders: dict[str, dict[str, SequenceLoader]]
+) -> dict[str, dict[str, list[TemporalModel]]]:
     temporal_models = {}
     for model_name in os.listdir(results_path):
         temporal_models[model_name] = {}
@@ -72,44 +65,30 @@ def create_models(results_path:str, loaders: dict[str, dict[str, SequenceLoader]
             loader = loaders[model_name][dataset_name]
             num_dims = loader.num_dims
             seq_len = loader.sequence_len
-            temporal_models[model_name][dataset_name] = create_models_list(input_size=num_dims, output_size=num_dims)
+            temporal_models[model_name][dataset_name] = create_models_list(
+                input_size=num_dims, output_size=num_dims
+            )
     return temporal_models
+
 
 def create_models_list(input_size: int, output_size: int) -> list[TemporalModel]:
     model_classes = [RNNModel, LSTMModel]
     hidden_sizes = [32, 64, 128]
     num_layers = [1, 2, 3]
     models = []
-    for model_class, hidden_size, num_layer in itertools.product(model_classes, hidden_sizes, num_layers):
-        model = model_class(input_size=input_size, hidden_size=hidden_size, num_layers=num_layer, output_size=output_size)
+    for model_class, hidden_size, num_layer in itertools.product(
+        model_classes, hidden_sizes, num_layers
+    ):
+        model = model_class(
+            input_size=input_size,
+            hidden_size=hidden_size,
+            num_layers=num_layer,
+            output_size=output_size,
+        )
         models.append(model)
 
     return models
 
-def get_encoder_model(results_path: str) -> dict[str, BaseNN]:
-    best_models = {}
-    for model_type in os.listdir(results_path):
-        for model_name in os.listdir(f"{results_path}/{model_type}"):
-            best_models[model_type] = {}
-            if model_name in ["_best_model"] or model_name.endswith("json"):
-                continue
-            model_base_name = "_".join(model_name.split("_")[0:-1])
-            model_params = load_model_yaml(model_base_name, yaml_path="src/conf/models")
-            break
-
-        for dataset_name in os.listdir(f"{results_path}/{model_type}/_best_model"):
-
-            dataset_dir = f"{results_path}/{model_type}/_best_model/{dataset_name}"
-            with open(f"{dataset_dir}/results.json") as file:
-                results = json.load(file)
-
-            params = results["params"]
-            params["optimizer"]["type"] = Adam if params["optimizer"]["type"] == "Adam" else SGD
-            num_channels = len(results["channels"])
-
-            model = create_encoder_model(model_params, model_base_name, num_channels, params, dataset_dir)
-            best_models[model_type][dataset_name] = model
-    return best_models
 
 if __name__ == "__main__":
     main()
