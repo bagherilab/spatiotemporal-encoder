f11effc83920c9d19be0ec2eb755b47db21b8421
Jacob Evarts <31416036+jacob-evarts@users.noreply.github.com>
2026-04-20 15:47:20 -0700

    merge conflict resolution

--- src/simulation_encoder/main.py
+++ src/simulation_encoder/main.py
@@ -242,19 +242,24 @@ def main() -> None:
     data_dir = config["data_dir"]
     model_configs = config["model_configs"]
 
+    num_epochs = model_configs["num_epochs"]
     batch_size = model_configs["batch_size"]
     test_split = model_configs["test_split"]
     verbose = model_configs["verbose"]
 
+    models = model_configs["models"]
+
     runner = Runner(exp_name, verbose)
 
     model_yaml_dir = "src/conf/models"
-    for model_yaml in os.listdir(model_yaml_dir):
-        if model_yaml.endswith(".yaml"):
-            runner.add_model(f"{model_yaml_dir}/{model_yaml}")
+    for model in models:
+        model_yaml = f"{model}.yaml"
+        if not os.path.exists(f"{model_yaml_dir}/{model_yaml}"):
+            raise FileNotFoundError(f"Model config file {model_yaml} not found in {model_yaml_dir}")
+        runner.add_model(f"{model_yaml_dir}/{model_yaml}")
 
     runner.add_dataset(data_dir, test_split, batch_size)
-    runner.run_models()
+    runner.run_models(num_epochs=num_epochs)
     runner.save_results()
 
 
